e0b7dafefc598db960265d9ed2bfb155901be01f
msttnakashima <72351631+msttnakashima@users.noreply.github.com>
2022-05-24 17:10:44 +0800

    edited interface of the app

--- app.py
+++ app.py
@@ -33,8 +33,7 @@ class SampleApp(Tk):
     def __init__(self, *args, **kwargs):
         Tk.__init__(self, *args, **kwargs)
 
-        # formatting purposes (set font properties)
-        self.title_font = tkfont.Font(family="Arial", size=18, weight="bold")   
+        self.title_font = tkfont.Font(family="Arial", size=18, weight="bold")   # formatting purposes (set font properties)
         self.subtitle_font = tkfont.Font(family = "Arial", size = 12, weight = "bold")
 
         # pages / frames are in a stack (frames that need to be visible will be raised above other frames)
@@ -45,7 +44,7 @@ class SampleApp(Tk):
 
         self.frames = {}
         # for F in (LandingPage, TasksMainPage, AboutPage, PageThree):
-        for F in (LandingPage, TasksMainPage, AboutPage):
+        for F in (LandingPage, TasksMainPage, AboutPage, AllTasksPage, AllCategoriesPage):
             page_name = F.__name__                              # get page name
             frame = F(parent=container, controller=self)        # frame
             self.frames[page_name] = frame
@@ -85,16 +84,18 @@ class LandingPage(Frame):
         button2.pack(side = "top", fill = "x", pady = 20)
         button3.pack(side = "top", fill = "x", pady = 20)
 
-# loadData - function to load tasks from database 
-def loadData(listbox_category, listbox_tasks): 
+# loadTasks - function to load all tasks from database 
+def loadTasks(listbox_tasks): 
     # select all tasks
-    dbCursor.execute("SELECT CONCAT(CAST(`duedate` AS CHAR), ': ', tasktitle, ' - ', description) FROM task")
+    dbCursor.execute("SELECT CONCAT(CAST(`duedate` AS CHAR), ': ', tasktitle, ' - ', description, ' | Status: ', status) FROM task")
     listbox_tasks.delete(0, END)
     i = 0
     for task in dbCursor: 
         for j in range(len(task)):
             listbox_tasks.insert(END, task[j])
 
+# loadTasks - function to load all categories from database 
+def loadCategories(listbox_category): 
     # select all categories
     dbCursor.execute("SELECT categoryname FROM category")
     listbox_category.delete(0, END)
@@ -112,16 +113,29 @@ class TasksMainPage(Frame):
 
         frame = Frame(self, parent)
 
-        # label for category and task listbox
-        label = Label(self, text="All categories \t\t All Tasks", font=controller.subtitle_font)
-        label.pack(side="top", fill= 'none', pady=10, anchor = NW)
+        # button to view all tasks
+        allTasksBtn = Button(self, text="View all tasks", width=48, command=lambda: controller.show_frame("AllTasksPage"))
+        allTasksBtn.pack(side = 'top', fill = 'x', pady = 60)  
 
-        # box to list all categories
-        listbox_category = Listbox(frame, height=15, width=20)
-        listbox_category.pack(side=LEFT)
+        # button to view all categories
+        allCategoriesBtn = Button(self, text="View all categories", width=48, command=lambda: controller.show_frame("AllCategoriesPage"))
+        allCategoriesBtn.pack(side = 'top', fill = 'x', pady = 60)  
+
+        menubutton = Button(self, text = "Go back to the main page", command=lambda: controller.show_frame("LandingPage"))
+        menubutton.pack(side = 'top', fill = 'x', pady = 60)
+
+        frame.pack()
+
+#AllTasks - view all tasks page
+class AllTasksPage(Frame): 
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        self.controller = controller
+
+        frame = Frame(self, parent)
 
         # box to list all tasks
-        listbox_tasks = Listbox(frame, height=15, width=50)
+        listbox_tasks = Listbox(frame, height=15, width=70)
         listbox_tasks.pack(side=LEFT)
 
         # scrollbar for tasks list 
@@ -132,33 +146,73 @@ class TasksMainPage(Frame):
         listbox_tasks.config(yscrollcommand=scrollbar_tasks.set)
         scrollbar_tasks.config(command=listbox_tasks.yview)
 
-        # load tasks from database 
-        loadDataBtn = Button(self, text="Load data", width=48, command = loadData(listbox_category, listbox_tasks))
-        loadDataBtn.pack(side = 'bottom')
-
-        # button to add a task
-        addTaskBtn = Button(self, text="Add task", width=48)
-        addTaskBtn.pack(side = 'bottom')
-
-        # button to delete a task (NOTE: a task can be selected from the listbox)
-        deleteTaskBtn = Button(self, text="Delete task", width=48)
-        deleteTaskBtn.pack(side = 'bottom')
-
-        # button to edit a task (NOTE: a task can be selected from the listbox)
-        editTaskBtn = Button(self, text = "Edit a task", width=48)
-        editTaskBtn.pack(side = 'bottom')
-
-        #button to mark a task as done (NOTE: a task can be selected from the listbox)
-        markDoneBtn = Button(self, text = "Mark as done", width=48)
-        markDoneBtn.pack(side = 'bottom')
+        # button to customize view of tasks
+        viewTasksByBtn = Button(self, text="View tasks by ...", width=48)
+        viewTasksByBtn.pack(side = 'bottom', fill = 'x')
 
         #button to add a task to a category
         addCategoryBtn = Button(self, text = "Add task to category", width=48)
-        addCategoryBtn.pack(side = 'bottom')
+        addCategoryBtn.pack(side = 'bottom', fill = 'x')
 
-        # button to customize view of tasks
-        viewTasksByBtn = Button(self, text="View tasks by ...", width=48)
-        viewTasksByBtn.pack(side = 'bottom')        
+        #button to search for a task
+        searchTaskBtn = Button(self, text = "Search for a task", width=48)
+        searchTaskBtn.pack(side = 'bottom', fill = 'x')
+
+        #button to mark a task as done (NOTE: a task can be selected from the listbox)
+        markDoneBtn = Button(self, text = "Mark as done", width=48)
+        markDoneBtn.pack(side = 'bottom', fill = 'x')
+
+        # button to edit a task (NOTE: a task can be selected from the listbox)
+        editTaskBtn = Button(self, text = "Edit a task", width=48)
+        editTaskBtn.pack(side = 'bottom', fill = 'x')
+
+        # button to delete a task (NOTE: a task can be selected from the listbox)
+        deleteTaskBtn = Button(self, text="Delete task", width=48)
+        deleteTaskBtn.pack(side = 'bottom', fill = 'x')
+
+        # button to add a task
+        addTaskBtn = Button(self, text="Add task", width=48)
+        addTaskBtn.pack(side = 'bottom', fill = 'x') 
+
+        # load tasks from database 
+        loadDataBtn = Button(self, text="Load data", width=48, command = loadTasks(listbox_tasks))
+        loadDataBtn.pack(side = 'bottom', fill = 'x')     
+
+        menubutton = Button(self, text = "Go back to the previous page", command=lambda: controller.show_frame("TasksMainPage"))
+        menubutton.pack(anchor = NE)
+
+        frame.pack()
+
+#AllCategoriesPage - view all categories page
+class AllCategoriesPage(Frame): 
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        self.controller = controller
+
+        frame = Frame(self, parent)
+
+        # box to list all tasks
+        listbox_category = Listbox(frame, height=15, width=70)
+        listbox_category.pack(side=LEFT)
+
+        # scrollbar for tasks list 
+        scrollbar_tasks = Scrollbar(frame)
+        scrollbar_tasks.pack(side=RIGHT, fill=Y)
+
+        # vertical scrollbar properties
+        listbox_category.config(yscrollcommand=scrollbar_tasks.set)
+        scrollbar_tasks.config(command=listbox_category.yview)
+
+        # button to customize view of tasks by category
+        viewTasksByBtn = Button(self, text="View tasks by category", width=48)
+        viewTasksByBtn.pack(side = 'bottom', fill = 'x')    
+
+        # load tasks from database 
+        loadDataBtn = Button(self, text="Load data", width=48, command = loadCategories(listbox_category))
+        loadDataBtn.pack(side = 'bottom', fill = 'x')  
+
+        menubutton = Button(self, text = "Go back to the previous page", command=lambda: controller.show_frame("TasksMainPage"))
+        menubutton.pack(anchor = NE)
 
         frame.pack()
 
